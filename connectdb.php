a1c364ebddbf3ca4c8ce441b37ada79c40fd9875
Mustafa Acıkaraoğlu <macikaraoglu13@ku.edu.tr>
2017-05-20 21:06:47 +0300

    Update connectdb.php

--- connectdb.php
+++ connectdb.php
@@ -1,8 +1,8 @@
 <?php
 $host = "localhost";
-$user = "group11";
-$password = "1491484841458554584";
-$dp = "group11";
+$user = "**********";
+$password = "**************";
+$dp = "**********";
 $con = mysqli_connect($host,$user,$password,$dp);
 if(!$con){
 	die("Error". mysqli_connect_error());
